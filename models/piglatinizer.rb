5a8a3b7c318dcba3ed951f6eb1ec23ffe1800050
Alouiscious Media <info@reneesoul.com>
2019-10-20 16:34:12 -0700

    Done.

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -1,5 +1,13 @@
 require 'pry'
 class PigLatinizer < Sinatra::Base
   
+  def can_piglatin?(text)
+    !text.match(/aeiouAEIOU/)
+  end
+
+  def prep_for_piglatin(text)
   binding.pry
+
+  end
+
 end
